ccefd9fc9214e7752affa01d86271f16bdc759df
dldvlpr <dormoy.dev@gmail.com>
2022-11-08 16:54:19 +0100

    first view artist now add more for display adjust

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Artist;
 use App\Form\ArtistModifyType;
+use App\Repository\ArtistRepository;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,21 +15,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/artist', name: 'artist_')]
 class ArtistController extends AbstractController
 {
-    #[Route('/edit/{id}', name: 'edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, UserRepository $userRepository, Artist $artist): Response
+    #[Route('/', name: 'showAll')]
+    public function showAll(ArtistRepository $artistRepository): Response
     {
-        $form = $this->createForm(ArtistModifyType::class, $artist);
-        $form->handleRequest($request);
+        $artists = $artistRepository->findAll();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $userRepository->save($artist, true);
-            return $this->redirectToRoute('user_index');
-        }
-
-        return $this->renderForm('artist/edit.html.twig', [
-            'form' => $form,
-            'user' => $artist
-        ]);
+        return $this->render('artist/index.html.twig', [
+            'artists' => $artists,
+            ]);
     }
+
 }
