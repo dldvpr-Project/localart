f0d2dfb544086ec8daf12f6c80da3f882fdaf735
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2022-12-05 17:27:23 +0100

    correction route new artCard

--- src/Controller/ArtCardController.php
+++ src/Controller/ArtCardController.php
@@ -33,7 +33,7 @@ class ArtCardController extends AbstractController
     }
 
     #[Route('/new', name: 'new')]
-    #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ARTIST")]
+    #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_ARTIST')")]
     public function new(Request $request, ArtCardRepository $artCardRepository): Response
     {
         $artCard = new ArtCard();
