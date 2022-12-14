ea7b2241a5e76e4cc3dbcaf79d758964aca6854d
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2022-12-14 10:59:23 +0100

    add picture for fixtures

--- src/DataFixtures/ArtCardFixtures.php
+++ src/DataFixtures/ArtCardFixtures.php
@@ -19,9 +19,9 @@ class ArtCardFixtures extends Fixture implements DependentFixtureInterface
             $artCard = new artCard();
             $artCard->setTitle($faker->word());
             $picture = $faker->randomElement([
-                '_fixtures_',
-                '_fixtures_',
-                '_fixtures_',
+                '_fixtures_art_1',
+                '_fixtures_art_2',
+                '_fixtures_art_3',
             ]);
             $artCard->setPictureArt($picture);
             $artCard->setDescription($faker->paragraph(5));
