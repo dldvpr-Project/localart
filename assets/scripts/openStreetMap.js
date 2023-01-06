250afb8e16b1c49e51e9efe0be9f4eff20aa9e91
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-01-06 15:49:14 +0100

    add map, rework css art

---
+++ assets/scripts/openStreetMap.js
@@ -0,0 +1,23 @@
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+import * as L from 'leaflet';
+
+
+let lat = 48.852969;
+let lon = 2.349903;
+let macarte = null;
+// Fonction d'initialisation de la carte
+function initMap() {
+    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+    macarte = L.map('map').setView([lat, lon], 11);
+    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20
+    }).addTo(macarte);
+}
+window.onload = function(){
+    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+    initMap();
+};
