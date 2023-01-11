d636389fcfe0cfebb462149528199a7e47b324f1
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-01-11 17:00:26 +0100

    remove useless use

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\ArtCard;
 use App\Repository\ViewRandCardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArtCardRepository;
-use App\Service\FindArtsByRand;
-
 
 #[Route('/', name: 'home_')]
 class HomeController extends AbstractController
